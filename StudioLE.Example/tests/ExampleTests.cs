c22c6ecb24af94715c95c10e22a306aa641887af
Laurence Elsdon <git@elsdon.me>
2023-12-12 08:58:45 +0000

    chore: Initial commit

---
+++ StudioLE.Example/tests/ExampleTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace StudioLE.Example.Tests;
+
+internal sealed class ExampleTests
+{
+    [Test]
+    public void Example_HelloWorld()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Pass();
+    }
+}
